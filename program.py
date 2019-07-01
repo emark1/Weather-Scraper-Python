78bc5038073ae61bd1856b55ada8b32ec2811674
emark1 <devel010@umn.edu>
2019-06-30 19:40:49 -0500

    Final

--- program.py
+++ program.py
@@ -9,7 +9,8 @@ def main():
     #get html from website
     html = get_url(code)
     #parse the html text
-    parse_html(html)
+    report = parse_html(html)
+    print("It is currently {} {} in {}. The conditions are {}.".format(report[2], report[3], report[1], report[0].lower()))
     #display forecast
 
 def print_header():
@@ -18,13 +19,30 @@ def print_header():
     print('-------')
 
 def get_url(zipcode):
-    url = 'https://www.wunderground.com/weather/us/tx/houston/{}'.format(zipcode)
+    url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
     response = requests.get(url)
     return response.text
 
 def parse_html(html):
     soup = bs4.BeautifulSoup(html, 'html.parser')
-    # print(soup)
+    loc = soup.find(class_='region-content-header').find('h1').get_text()
+    condition = soup.find(class_='condition-icon').get_text()
+    temp = soup.find(class_='wu-unit-temperature').find(class_='wu-value').get_text()
+    scale = soup.find(class_='wu-unit-temperature').find(class_='wu-label').get_text()
+
+    loc = cleanup_text(loc)
+    condition = cleanup_text(condition)
+    temp = cleanup_text(temp)
+    scale = cleanup_text(scale)
+
+    return condition, loc, temp, scale
+
+def cleanup_text(text : str):
+    if not text:
+        return text
+    
+    text = text.strip()
+    return text
 
 if __name__ == '__main__':
     main()
